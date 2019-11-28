a93597909cda1a6d217994ff09aa116867d4ad9d
M. Sonntag <michael.p.sonntag@gmail.com>
2019-11-28 16:05:12 +0100

    [test/datacite] Add dict_from_xml

--- test/test_importers_datacite.py
+++ test/test_importers_datacite.py
@@ -1,15 +1,32 @@
+"""
+test_importers_datacite tests the basic functions
+and methods of the DataCite importer script.
+"""
+
+import os
 import unittest
 
-from odmltools.importers.import_datacite import camel_to_snake
+import odmltools.importers.import_datacite as dimp
 
 
 class TestSection(unittest.TestCase):
     def setUp(self):
-        pass
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.resources = os.path.join(dir_path, "resources")
 
     def test_camel_to_snake(self):
         camel = "GeoLocationPoint"
         snake = "geo_location_point"
 
-        conv = camel_to_snake(camel)
+        conv = dimp.camel_to_snake(camel)
         self.assertEqual(conv, snake)
+
+    def test_dict_from_xml(self):
+        invalid_file = os.path.join(self.resources, "noxml.md")
+        valid_file = os.path.join(self.resources, "load.xml")
+
+        with self.assertRaises(dimp.ParserException):
+            _ = dimp.dict_from_xml(invalid_file)
+
+        res_dict = dimp.dict_from_xml(valid_file)
+        self.assertIsInstance(res_dict, dict)
