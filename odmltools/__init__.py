41717b35a329abe38a62893959a44b4cd8ed8587
M. Sonntag <michael.p.sonntag@gmail.com>
2020-02-17 14:49:54 +0100

    [init] Make python_version func private

--- odmltools/__init__.py
+++ odmltools/__init__.py
@@ -1,10 +1,10 @@
 import warnings
 
-from sys import version_info as python_version
+from sys import version_info as _python_version
 
 from .info import VERSION
 
-if python_version.major < 3 or python_version.major == 3 and python_version.minor < 6:
+if _python_version.major < 3 or _python_version.major == 3 and _python_version.minor < 6:
     msg = "The '%s' package is not tested with your Python version. " % __name__
     msg += "Please consider upgrading to the latest Python distribution."
     warnings.warn(msg)
